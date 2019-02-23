f569e528399d5e968901465471429129b8a5887b
LamyaHanani <47285317+LamyaHanani@users.noreply.github.com>
2019-02-23 22:16:29 +0200

    Add files via upload

---
+++ src/homework/java/product.java
@@ -0,0 +1,9 @@
+package homework.java;
+
+public class product {
+
+	public int quantity;
+	public int price;
+	public String name;
+
+}
---
+++ src/homework/java/sc.java
@@ -0,0 +1,17 @@
+package homework.java;
+
+public class sc {
+
+	public
+ int count=0;
+	public int total=0;
+
+	public void addproduct(product r) {
+		// TODO Auto-generated method stub
+		count+= r.quantity;
+		total+= r.price;
+	}
+
+	
+
+}
